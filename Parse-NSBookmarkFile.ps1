7d2c1036094e884b0275050ded23c481855163fc
Sean Harper <seharper@microsoft.com>
2021-03-18 13:00:44 -0400

    Fixed bookmark handler

--- Parse-NSBookmarkFile.ps1
+++ Parse-NSBookmarkFile.ps1
@@ -4,3 +4,5 @@ switch -regex ($bookmarks.GetEnumerator()) {
     '^\s{0,40}\<DT\>\<A\sHREF\=\"(?<url>\S*)\"\sADD_DATE\=\"\d+\"\sICON\=\"\S*\"\>(?<title>.*)\<\/A\>$' {Write-Host "Match with icon: $($Matches.url)"}
     '^\s{0,40}\<DT\>\<A\sHREF\=\"(?<url>\S*)\"\sADD_DATE\=\"\d+\">(?<title>.*)\<\/A\>$' {Write-Host "Match without icon: $($Matches.url)"}
 }
+
+# All set
