7f2bbc44aaeb9e7b16af14513cce3f60a3dc7020
Sean Harper <seharper@microsoft.com>
2021-08-06 14:54:01 -0400

    Table updates

--- Parse-NSBookmarkFile.ps1
+++ Parse-NSBookmarkFile.ps1
@@ -1,4 +1,4 @@
-$bookmarks = Get-Content .\bookmarks_2_18_21.html
+$bookmarks = Get-Content .\favorites_8_6_21.html
 
 switch -regex ($bookmarks.GetEnumerator()) {
     '^\s{0,40}\<DT\>\<A\sHREF\=\"(?<url>\S*)\"\sADD_DATE\=\"\d+\"\sICON\=\"\S*\"\>(?<title>.*)\<\/A\>$' {Write-Host "Match with icon: $($Matches.url)"}
